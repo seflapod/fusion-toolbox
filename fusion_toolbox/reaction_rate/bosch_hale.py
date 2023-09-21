4f745fc925a8bba02a6e2c96bc9e7022168ce643
tmarkj <tmarkj@mit.edu>
2023-09-21 09:40:32 -0400

    Addressed code review comments and added more (working) tests

--- fusion_toolbox/reaction_rate/bosch_hale.py
+++ fusion_toolbox/reaction_rate/bosch_hale.py
@@ -1,69 +1,67 @@
 import numpy as np
-import sys
 import matplotlib.pyplot as plt
 
-## Class for Bosch-Hale reactivity
-## All of it comes from: H.-S. Bosch and G.M. Hale 1992 Nucl. Fusion 32 611
-class Reactivity:
+class Bosch_Hale_reactivity:
+    """Class for Bosch Hale reactivity (H.-S. Bosch and G.M. Hale 1992 Nucl. Fusion 32 611)
 
-	known_reactions = ["DDn", "DDp", "DTn", "D3Hep"]
+    Parameters
+    ----------
+    reaction : string
+        String name for the fusion reaction
 
-	# The space is here so the indexing matches the paper
-	DDn_Cs   = [" ", 5.43360e-12, 5.85778e-3, 7.68222e-3, 0.0, -2.96400e-6, 0.0, 0.0]
-	DDp_Cs   = [" ", 5.65718e-12, 3.41267e-3, 1.99167e-3, 0.0, 1.05060e-5, 0.0, 0.0]
-	DTn_Cs   = [" ", 1.1302e-9, 1.51361e-2, 7.51886e-2, 4.60643e-3, 1.35e-2, -1.0675e-4, 1.366e-5]
-	D3Hep_Cs = [" ", 5.51036e-10, 6.41918e-3, -2.02896e-3, -1.91080e-5, 1.35776e-4, 0.0, 0.0 ]
+    """
+    known_reactions = ["DDn", "DDp", "DTn", "D3Hep"]
 
-	DDn_Bg   = 31.3970
-	DDp_Bg   = 31.3970
-	DTn_Bg   = 34.3827
-	D3Hep_Bg = 68.7508
+    _DDn_Cs   = [5.43360e-12, 5.85778e-3, 7.68222e-3, 0.0, -2.96400e-6, 0.0, 0.0]
+    _DDp_Cs   = [5.65718e-12, 3.41267e-3, 1.99167e-3, 0.0, 1.05060e-5, 0.0, 0.0]
+    _DTn_Cs   = [1.1302e-9, 1.51361e-2, 7.51886e-2, 4.60643e-3, 1.35e-2, -1.0675e-4, 1.366e-5]
+    _D3Hep_Cs = [5.51036e-10, 6.41918e-3, -2.02896e-3, -1.91080e-5, 1.35776e-4, 0.0, 0.0]
 
-	DDn_mr   = 937814
-	DDp_mr   = 937814
-	DTn_mr   = 1124656
-	D3Hep_mr = 1124572
+    _DDn_Bg = 31.3970
+    _DDp_Bg = 31.3970
+    _DTn_Bg = 34.3827
+    _D3Hep_Bg = 68.7508
 
-	Cs_dict = {"DDn" : DDn_Cs, "DDp" : DDp_Cs, "DTn" : DTn_Cs, "D3Hep" : D3Hep_Cs}
+    _DDn_mr = 937814
+    _DDp_mr = 937814
+    _DTn_mr = 1124656
+    _D3Hep_mr = 1124572
 
-	Bg_dict = {"DDn" : DDn_Bg, "DDp" : DDp_Bg, "DTn" : DTn_Bg, "D3Hep" : D3Hep_Bg}
-	mr_dict = {"DDn" : DDn_mr, "DDp" : DDn_mr, "DTn" : DTn_mr, "D3Hep" : D3Hep_mr}
+    _Cs_dict = {"DDn":_DDn_Cs, "DDp":_DDp_Cs, "DTn":_DTn_Cs, "D3Hep":_D3Hep_Cs}
+    _Bg_dict = {"DDn":_DDn_Bg, "DDp":_DDp_Bg, "DTn":_DTn_Bg, "D3Hep":_D3Hep_Bg}
+    _mr_dict = {"DDn":_DDn_mr, "DDp":_DDn_mr, "DTn":_DTn_mr, "D3Hep":_D3Hep_mr}
 
-	def __init__(self, reaction):
+    def __init__(self, reaction):
 
-		if reaction in self.known_reactions:
-			self.reaction = reaction
-		else:
-            # THIS IS HORRIBLE PLZ FIX LATER!!!
-			print("Unknown reaction error.")
-			sys.exit()
+        if reaction in self.known_reactions:
+            self.reaction = reaction
+        else:
+            raise ValueError(f"Unknown reaction. Should be one of {self.known_reactions}")
 
-		self.C  = self.Cs_dict[self.reaction]
-		self.Bg = self.Bg_dict[self.reaction]
-		self.mr = self.mr_dict[self.reaction]
 
-	def get_reactivity(self, Temp):
+    def get_reactivity(self, T_i):
+        """Method to calculate the Bosch-Hale reactivity for the reaction
 
-		fraction_top    = Temp*(self.C[2] + Temp*(self.C[4] + Temp*self.C[6]))
-		fraction_bottom = 1 + Temp*(self.C[3] + Temp*(self.C[5] + Temp*self.C[7]))
+        Parameters
+        ----------
+        T_i : float
+            Ion temperature in keV for the fusion reactivity
 
-		theta = Temp/(1 - fraction_top/fraction_bottom)
-		xi    = (self.Bg**2/(4*theta))**(1/3)
+        Returns
+        -------
+        sigmaV : float
+            Calculated Bosch-Hale parameterized in cm^3/s 
+        """
+        C  = self._Cs_dict[self.reaction]
+        Bg = self._Bg_dict[self.reaction]
+        mr = self._mr_dict[self.reaction]
+        fraction_top    = T_i*(C[1] + T_i*(C[3] + T_i*C[5]))
+        fraction_bottom = 1 + T_i*(C[2] + T_i*(C[4] + T_i*C[6]))
 
-		sigmaV = self.C[1]*theta*np.sqrt(xi/(self.mr*Temp**3))*np.exp(-3*xi)
+        theta = T_i/(1 - fraction_top/fraction_bottom)
+        xi    = (Bg**2/(4*theta))**(1/3)
 
-		return sigmaV
+        sigmaV = C[0]*theta*np.sqrt(xi/(mr*T_i**3))*np.exp(-3*xi)
 
-	def plot_reactivity(self):
-
-		T_i = np.logspace(-1, 2, 1000)
-
-		fig, ax = plt.subplots(1,1)
-
-		ax.loglog(T_i, [self.get_reactivity(Temp) for Temp in T_i])
-		ax.set_title(f"Reactivity plot of {self.reaction}")
-		ax.set_ylabel(r"Reactivity (cm$^3$/s)")
-		ax.set_xlabel("Ion Temperature (keV)")
-
-		plt.show()
+        return sigmaV
 
--- test/test_reactivities_bosch_hale.py
+++ test/test_reactivities_bosch_hale.py
@@ -1,8 +1,66 @@
-import fusion_toolbox.reaction_rate.bosch_hale as bh
 import numpy.testing as npt
+import pytest
 
-def test_DTn_reactivity():
-    DTn_r= bh.Reactivity("DDp")
-    test_result= 1.3e-17 ## from NRL plasma formulary 
-    test_input = 5.0 # keV
-    npt.assert_almost_equal(DTn_r.get_reactivity(test_input), test_result, decimal=2) 
+## Testing DTn fusion reaction
+## Tested according to table VIII in H.-S. Bosch and G.M. Hale 1992 Nucl. Fusion 32 611
+@pytest.mark.parametrize(
+    "test, expected, precision",
+    [
+        (1.0, 6.857e-21, 21),
+        (2.0, 2.977e-19, 19),
+        (5.0, 1.366e-17, 18),
+        (10.0, 1.136e-16, 17),
+        (20.0, 4.330e-16, 16),
+    ])
+def test_DTn_reactivity(test, expected, precision):
+    import fusion_toolbox.reaction_rate.bosch_hale as bh
+    DTn_r= bh.Bosch_Hale_reactivity("DTn")
+    npt.assert_almost_equal(DTn_r.get_reactivity(test), expected, decimal=precision) 
+
+## Testing DDn fusion reaction
+## Tested according to table VIII in H.-S. Bosch and G.M. Hale 1992 Nucl. Fusion 32 611
+@pytest.mark.parametrize(
+    "test, expected, precision",
+    [
+        (1.0, 9.933e-23, 24),
+        (2.0, 3.110e-21, 22),
+        (5.0, 9.128e-20, 21),
+        (10.0, 6.023e-19, 20),
+        (20.0, 2.603e-18, 19),
+    ])
+def test_DDn_reactivity(test, expected, precision):
+    import fusion_toolbox.reaction_rate.bosch_hale as bh
+    DDn_r= bh.Bosch_Hale_reactivity("DDn")
+    npt.assert_almost_equal(DDn_r.get_reactivity(test), expected, decimal=precision) 
+
+## Testing DDp fusion reaction
+## Tested according to table VIII in H.-S. Bosch and G.M. Hale 1992 Nucl. Fusion 32 611
+@pytest.mark.parametrize(
+    "test, expected, precision",
+    [
+        (1.0, 1.017e-22, 23),
+        (2.0, 3.150e-21, 22),
+        (5.0, 9.024e-20, 21),
+        (10.0, 5.781e-19, 20),
+        (20.0, 2.399e-18, 20),
+    ])
+def test_DDp_reactivity(test, expected, precision):
+    import fusion_toolbox.reaction_rate.bosch_hale as bh
+    DDp_r= bh.Bosch_Hale_reactivity("DDp")
+    npt.assert_almost_equal(DDp_r.get_reactivity(test), expected, decimal=precision) 
+
+## Testing D3Hep fusion reaction
+## Tested according to table VIII in H.-S. Bosch and G.M. Hale 1992 Nucl. Fusion 32 611
+@pytest.mark.parametrize(
+    "test, expected, precision",
+    [
+        (1.0, 3.057e-26, 27),
+        (2.0, 1.399e-23, 24),
+        (5.0, 6.377e-21, 22),
+        (10.0, 2.126e-19, 20),
+        (20.0, 3.482e-18, 22),
+    ])
+def test_D3Hep_reactivity(test, expected, precision):
+    import fusion_toolbox.reaction_rate.bosch_hale as bh
+    D3Hep_r= bh.Bosch_Hale_reactivity("D3Hep")
+    npt.assert_almost_equal(D3Hep_r.get_reactivity(test), expected, decimal=precision) 
