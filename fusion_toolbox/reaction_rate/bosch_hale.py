6e8181d20090b5ee6cf75efc2ee198de7d8cc5f7
tmarkj <tmarkj@mit.edu>
2023-09-21 11:04:43 -0400

    Fixed PEP8 compliance issues

--- fusion_toolbox/reaction_rate/bosch_hale.py
+++ fusion_toolbox/reaction_rate/bosch_hale.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class BoschHaleReactivity:
     """Class for Bosch Hale reactivity (H.-S. Bosch and G.M. Hale 1992 Nucl. Fusion 32 611)
 
@@ -11,7 +12,7 @@ class BoschHaleReactivity:
     """
     known_reactions = ["DDn", "DDp", "DTn", "D3Hep"]
 
-    ## The values for C in the paper are the fit variables for the theta parameter shown in equation 13
+    ## The values for C in the paper are the fit variables for equation 13
     ## The values are from table VII.
     _DDn_Cs = [5.43360e-12, 5.85778e-3, 7.68222e-3, 0.0, -2.96400e-6, 0.0, 0.0]
     _DDp_Cs = [5.65718e-12, 3.41267e-3, 1.99167e-3, 0.0, 1.05060e-5, 0.0, 0.0]
@@ -43,13 +44,12 @@ class BoschHaleReactivity:
         else:
             raise ValueError(f"Unknown reaction. Should be one of {self.known_reactions}")
 
-
-    def get_reactivity(self, T_i):
+    def get_reactivity(self, ion_temperature):
         """Method to calculate the Bosch-Hale reactivity for the reaction
 
         Parameters
         ----------
-        T_i : float
+        ion_temperature : float
             Ion temperature in keV for the fusion reactivity
 
         Returns
@@ -60,12 +60,12 @@ class BoschHaleReactivity:
         C  = self._Cs_dict[self.reaction]
         Bg = self._Bg_dict[self.reaction]
         mr = self._mr_dict[self.reaction]
-        fraction_top = T_i*(C[1] + T_i*(C[3] + T_i*C[5]))
-        fraction_bottom = 1 + T_i*(C[2] + T_i*(C[4] + T_i*C[6]))
+        fraction_top = ion_temperature*(C[1] + ion_temperature*(C[3] + ion_temperature*C[5]))
+        fraction_bottom = 1 + ion_temperature*(C[2] + ion_temperature*(C[4] + ion_temperature*C[6]))
 
-        theta = T_i/(1 - fraction_top/fraction_bottom)
+        theta = ion_temperature/(1 - fraction_top/fraction_bottom)
         xi = (Bg**2/(4*theta))**(1/3)
 
-        sigma_v = C[0]*theta*np.sqrt(xi/(mr*T_i**3))*np.exp(-3*xi)
+        sigma_v = C[0]*theta*np.sqrt(xi/(mr*ion_temperature**3))*np.exp(-3*xi)
 
         return sigma_v
